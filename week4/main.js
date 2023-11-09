f517fd63a12e0ef13d37f2fe333aaa10994693c5
leressa-b <145095973+leressa-b@users.noreply.github.com>
2023-11-09 16:23:03 +0400

    Delete week4/main.js

--- week4/main.js
+++
@@ -1,7 +0,0 @@
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://leressa-b.github.io/F28WP/week4/cities1.json');
-ourRequest.onload = function() {
-var ourData = ourRequest.responseText;
-onsole.log(ourData[0]);
-};
-ourRequest.send();
