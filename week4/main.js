5ea92704b57cd578e0106b0f348e578bc98efa36
leressa-b <lnb2000@hw.ac.uk>
2023-11-09 16:26:22 +0400

    added the files in a sub folder

--- week4/main.js
+++
@@ -1,6 +0,0 @@
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://leressa-b.github.io/F28WP/week4/cities1.json');
-ourRequest.onload = function() {
-console.log(ourRequest.responseText);
-};
-ourRequest.send();
